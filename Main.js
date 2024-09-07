6cad725c2d0a1dab8e7799d7fd043a0591f1d981
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-08 01:23:31 +0500

    Que 20: Empty Array

---
+++ Main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Que 20: Empty Array: Think of something you could store in a array. For example, you could make a list of mountains, 
+// rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these 
+// items.
+Object.defineProperty(exports, "__esModule", { value: true });
+// ***************************************************************************************************************************
+// Think of something you could store in a array.
+let items = ["Pakistan", "Karachi", "Urdu"];
+// Write a program that creates a list containing these items.
+let items2 = [];
+items2.push("Germany");
+items2.push("England");
+items2.push("India");
+console.log(items2);
---
+++ Main.ts
@@ -0,0 +1,17 @@
+// Que 20: Empty Array: Think of something you could store in a array. For example, you could make a list of mountains, 
+// rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these 
+// items.
+
+// ***************************************************************************************************************************
+
+// Think of something you could store in a array.
+
+let items: string[] = ["Pakistan" , "Karachi" , "Urdu"];
+
+// Write a program that creates a list containing these items.
+
+let items2: string[] = [];
+items2.push("Germany");
+items2.push("England");
+items2.push("India");
+console.log(items2);
